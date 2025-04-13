5f10b50a20fbe6ee54359bff277547b57dd95fd1
Kimsongbeen <ksb10266@gmail.com>
2025-04-13 21:10:30 +0900

    auth-service_1.1v_완료

--- src/main/java/com/nhnacademy/common/exception/CommonHttpException.java
+++ src/main/java/com/nhnacademy/common/exception/CommonHttpException.java
@@ -1,15 +1,21 @@
 package com.nhnacademy.common.exception;
 
+import lombok.Getter;
+
 /**
- * HTTP 오류를 처리하는 기본 예외 클래스입니다.
+ * HTTP 상태 코드와 메시지를 함께 포함할 수 있는 사용자 정의 예외 클래스입니다.
  * <p>
- * 이 클래스는 상태 코드와 오류 메시지를 포함하여 HTTP 오류와 관련된 예외를 처리하는 데 사용됩니다.
- * </p>
+ * 주로 HTTP 오류 응답을 처리할 때 사용되며, 상태 코드와 예외 메시지를 함께 전달할 수 있습니다.
  */
 public class CommonHttpException extends RuntimeException {
 
     /**
      * HTTP 상태 코드 (예: 400, 404, 500 등)
+     * -- GETTER --
+     *  저장된 HTTP 상태 코드를 반환합니다.
+     *
+     * @return HTTP 상태 코드
+
      */
     private final int statusCode;
 
@@ -25,11 +31,11 @@ public class CommonHttpException extends RuntimeException {
     }
 
     /**
-     * 주어진 상태 코드, 메시지 및 원인으로 {@link CommonHttpException} 예외를 생성합니다.
+     * 상태 코드, 메시지, 원인 예외(cause)를 기반으로 예외 객체를 생성합니다.
      *
      * @param statusCode HTTP 상태 코드
-     * @param message    오류 메시지
-     * @param cause      예외의 원인
+     * @param message    예외 메시지
+     * @param cause      원인 예외
      */
     public CommonHttpException(final int statusCode, final String message, final Throwable cause) {
         super(message, cause);
